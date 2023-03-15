89698e678a8b6ffefd748d9fb6ea576e5e095463
Roypc86 <roypc86@gmail.com>
2023-03-15 09:22:57 -0600

    Agregado de radio buttons para la selección de la restricción de edad. Autoocultado de la opcion de acciones según la restricción de edad.

---
+++ WebApp_Cinepolis/WebApp_Cinepolis/Scripts/PeliculasScript.js
@@ -0,0 +1,28 @@
+﻿var textarea = document.querySelector('textarea');
+
+textarea.addEventListener('keydown', autosize);
+document.addEventListener("DOMContentLoaded", clickRadioButtonAdultos);
+
+function autosize() {
+    var el = this;
+    setTimeout(function () {
+        el.style.cssText = 'height: fit-content; padding:0; width: 100%;';
+        el.style.cssText = 'height:' + el.scrollHeight + 'px; width: 100%;';
+        
+    }, 0);
+}
+
+
+function clickRadioButtonAdultos() {
+
+    //Obtención de los elementos de la edición del Juguete
+    var divAcciones = document.getElementById("form_acciones");
+    console.log($("#AdultosRadio").prop('checked'))
+    // Ocultado de elementos según el tipo de combo que sea
+    if ($("#AdultosRadio").prop('checked')) {//Caso es para adulto
+        divAcciones.style = "display: block;";
+
+    } else {//Caso es para niños
+        divAcciones.style = "display: none;";
+    }
+}
