ae672dc5e15a76446bf40b9b6114f185ebeeffe8
Roypc86 <roypc86@gmail.com>
2023-03-18 13:54:43 -0600

    Api database

---
+++ API/API_Database/API_Database/Director.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Director]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
+    [Nombre] NVARCHAR(60) NULL, 
+    [Biografia] NVARCHAR(200) NULL
+)
---
+++ API/API_Database/API_Database/Script.PostDeployment1.sql
@@ -0,0 +1,21 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+MERGE INTO Director AS Target
+USING (VALUES
+ (1, 'James Cameron', 'Biografía de James Cameron'),
+ (2, 'Chris Miller', 'Biografía de Chris Miller ')
+)
+AS Source ([Id], Nombre, Biografia)
+ON Target.Id = Source.Id
+WHEN NOT MATCHED BY TARGET THEN
+INSERT (Nombre, Biografia)
+VALUES (Nombre, Biografia);
